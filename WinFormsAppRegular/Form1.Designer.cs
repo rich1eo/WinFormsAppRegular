fd0cb6634a8808f09436eee0d006bf548fe9ac74
richkov <richkovvladislav@yandex.ru>
2020-10-06 21:09:52 +0300

    Initial commit

---
+++ WinFormsAppRegular/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WinFormsAppRegular
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.wordout = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выберите файл\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wordout
+            // 
+            this.wordout.Location = new System.Drawing.Point(73, 144);
+            this.wordout.Multiline = true;
+            this.wordout.Name = "wordout";
+            this.wordout.Size = new System.Drawing.Size(240, 110);
+            this.wordout.TabIndex = 1;
+            this.wordout.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(121, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилии лиц выше 190 см\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wordout);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox wordout;
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ WinFormsAppRegular/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WinFormsAppRegular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            Regex regex = new Regex(@"^([А-Я][а-я]+)\s+([А-Я])\.\s*([А-Я])\.\s+(\d+)\s+(\d+)$");
+            using (StreamReader reader = new StreamReader(file.FileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    Match match = regex.Match(reader.ReadLine() ?? "");
+                    if (match.Success && int.Parse(match.Groups[4].Value) > 190) {
+                        wordout.Text += (match.Groups[1].Value) + Environment.NewLine;
+                    }
+                }
+            }
+        }
+    }
+}
